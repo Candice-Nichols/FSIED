831579bd653dceae93bd9e8bf11ce9a274362ca8
Caitie <caitie.mccafferty@gmail.com>
2019-02-15 13:14:36 -0600

    added script for EM volume

--- chimera_volume.py
+++ chimera_volume.py
@@ -5,11 +5,11 @@ from chimera import replyobj # emits status message
 from chimera.tkgui import saveReplyLog
 
 
-fn=("pdb5m3l.pdb") # change to file name 
+fn=("6he8.pdb") # change to file name 
 
 replyobj.status('Processing ' + fn) # shows the file that is being worked on
 runCommand('open ' + fn)
-runCommand('surf #0')
+runCommand('surf #0 grid 0.5')
 runCommand('measure volume #0')
 runCommand('measure area #0')
 runCommand('close all')
