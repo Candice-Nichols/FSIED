82a54aaf9c787ea82e09dd4c0b091c4f8a10e4d1
candice_chen_96 <candice.chen@utexas.edu>
2019-04-11 12:38:37 -0500

    added fetchpdb.py

---
+++ fetchpdb.py
@@ -0,0 +1,13 @@
+from IPython.display import HTML
+from pypdb import *
+import pprint
+
+
+pdbids=input("Please enter pdb ID, separated by comma: ")
+pdbids=pdbids.split(",")
+for id in pdbids:
+	pdb_file= get_pdb_file(id, filetype='pdb', compression=False)
+	filename = id + ".pdb"
+	f = open(filename, "w")
+	f.write(pdb_file)
+print(pdbids)
