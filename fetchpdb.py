dffd512bbd5c9451645052743f4710a4a738b569
candice_chen_96 <candice.chen@utexas.edu>
2019-04-13 23:15:13 -0500

    added exceptionss

--- fetchpdb.py
+++ fetchpdb.py
@@ -2,13 +2,20 @@ from IPython.display import HTML
 from pypdb import *
 import pprint
 
-pdbids=input("Please enter pdb ID, separated by comma: ")
+#Prompt users to enter pdb IDs
+pdbids=input("Please enter pdb ID(s), separated by comma: ")
 pdbids=pdbids.split(",")
+
+#Fetch pdb files corresponding to the user input
 for id in pdbids:
-	pdb_file= get_pdb_file(id, filetype='pdb', compression=False)
-	filename = id + ".pdb"
-	f = open(filename, "w")
-	f.write(pdb_file)
+	try:
+		pdb_file= get_pdb_file(id, filetype='pdb', compression=False)
+		filename = id + ".pdb"
+		f = open(filename, "w")
+		f.write(pdb_file)
+	except:
+		print("Sorry! The pdb ID "+ id + " does not exist!")
+		exit()
 print(pdbids)
 
 
--- main.sh
+++ main.sh
@@ -1,8 +1,20 @@
+#!/bin/bash
+
 python fetchpdb.py
 
-./3vvolume.sh
 
-for file in *.pdb
-do
-	rm $file
-done
+
+#execute 3vvolume.sh only when pdb files are found in the directory
+if [ ! -f *.pdb ]; then
+    echo "file(s) not found"
+    exit 1
+else
+    echo "the file(s) exist"
+    ./3vvolume.sh
+    
+    for file in *.pdb
+        do
+            rm $file
+        done
+fi
+
